7626bc0a8de69726154e49bd3e0627682fea0a6a
benjaminpants <maltbyfamily@gmail.com>
2021-12-17 23:30:41 -0800

    new save system lol

--- Assets/Scripts/SaveClasses.cs
+++ Assets/Scripts/SaveClasses.cs
@@ -1,10 +1,11 @@
 using System;
 
-[Serializable]
 public class Settings
 {
     public float volume = 0.5f;
     public float sens = 100f;
+
+	public byte version = 0;
 
     public bool hasWon = false;
 }
--- Assets/Scripts/SaveManager.cs
+++ Assets/Scripts/SaveManager.cs
@@ -11,12 +11,18 @@ public class SaveManager : MonoBehaviour
     public static string directory = PlayerPrefs.GetString("PlayerName");
     public static string filename = "settings.davedata";
 
-    public static void SaveSettings(Settings settings)
+	public const byte LatestSaveVersion = 0;
+
+	public static void SaveSettings(Settings settings)
     {
-        BinaryFormatter bf = new BinaryFormatter();
-        FileStream fs = File.Create(GetFullPath());
-        bf.Serialize(fs, settings);
-        fs.Close();
+        FileStream fs = File.Open(GetFullPath(),FileMode.OpenOrCreate);
+		BinaryWriter br = new BinaryWriter(fs);
+		br.Write((byte)settings.version);
+		br.Write(settings.volume);
+		br.Write(settings.sens);
+		br.Write(settings.hasWon);
+		
+		fs.Close();
     }
     public static Settings LoadSettings()
     {
@@ -24,10 +30,30 @@ public class SaveManager : MonoBehaviour
         {
             try
             {
-                BinaryFormatter bf = new BinaryFormatter();
-                FileStream fs = File.Open(GetFullPath(), FileMode.Open);
-                Settings settings = (Settings)bf.Deserialize(fs);
-                fs.Close();
+                FileStream fs = File.Open(GetFullPath(), FileMode.OpenOrCreate);
+				BinaryReader br = new BinaryReader(fs);
+				Settings settings = new Settings();
+				settings.version = br.ReadByte();
+				if (settings.version != LatestSaveVersion)
+				{
+					switch (settings.version) //implement legacy reading for old file versiolns and then resave the file or whatever, rn though 0 is the only version
+					{
+						default:
+							throw new System.Exception("Save File out of date!");
+					}
+				}
+				else
+				{
+					settings.version = LatestSaveVersion;
+					settings.volume = br.ReadSingle();
+					settings.sens = br.ReadSingle();
+					settings.hasWon = br.ReadBoolean();
+				}
+				Debug.Log(settings.version);
+				Debug.Log(settings.volume);
+				Debug.Log(settings.sens);
+				Debug.Log(settings.hasWon);
+				fs.Close();
 
                 return settings;
             }
